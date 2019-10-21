72197d32e863a5434689127ae1aba5fe3a848b8c
github username <github email address>
2019-10-21 11:31:24 -0700

    working on serializer

--- idle_halloween_api/app/serializers/user_serializer.rb
+++ idle_halloween_api/app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
+  has_many :items, through: :inventory
   attributes :id, :level, :hp, :att
 end
--- idle_halloween_api/db/schema.rb
+++ idle_halloween_api/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_185749) do
+ActiveRecord::Schema.define(version: 2019_10_21_180804) do
+
+  create_table "inventories", force: :cascade do |t|
+    t.integer "user_id_id"
+    t.integer "item_id_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id_id"], name: "index_inventories_on_item_id_id"
+    t.index ["user_id_id"], name: "index_inventories_on_user_id_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
