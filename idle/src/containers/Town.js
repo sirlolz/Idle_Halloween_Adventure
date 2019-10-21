d7476b8657a040978d1c31b762e0159815753a55
github username <github email address>
2019-10-21 16:34:29 -0700

    merging

--- idle/src/containers/Town.js
+++ idle/src/containers/Town.js
@@ -16,6 +16,7 @@ class Town extends React.Component{
 
     handleClick = () => {
         this.setState({stable: !this.state.stable})
+        
     }
     render(){
 
@@ -23,6 +24,7 @@ class Town extends React.Component{
             return <Stables />
         }
         return(    
+            <Router>
             <div className="gallery">
                 <figure className="gallery__item gallery__item--1">
                     <Link to="/blacksmithstore">
@@ -34,21 +36,21 @@ class Town extends React.Component{
                 </figure> 
 
                 <figure className="gallery__item gallery__item--2">
-                    {/* <Route to="/taverninn" component={TavernInn}/> */}
+                    <Route to="/taverninn" component={TavernInn}/>
                     <img src="https://png2.cleanpng.com/sh/402783dc704200a9b1f672db8b1c06d0/L0KzQYm3VMAyN5DuiZH0aYP2gLBuTflvdl51jdQ2Y3zsgH7okwQudZZpgddBYXywRbLqUMk5OGo5UaptNUCxRYi5U8k5OWo2TaQ7NUe0Qoi6U8AyO191htk=/kisspng-inn-pub-clip-art-medieval-5ac09809498d50.5723981915225712733013.png" 
                     className="gallery__img" 
                     alt="taverninn"/>
                 </figure>
 
                 <figure className="gallery__item gallery__item--3">
-                    {/* <Route to="/stables" component={Stables}/> */}
+                    <Route to="/stables" component={Stables}/>
                     <img onClick={() =>this.handleClick()}src="https://cdn.pixabay.com/photo/2012/05/07/17/05/building-48709_960_720.png" 
                     className="gallery__img" 
                     alt="stables"/>
                 </figure>
 
                 <figure className="gallery__item gallery__item--4">
-                    {/* <Route to="/blacksmithstore" component={BlacksmithStore}/> */}
+                    <Route to="/blacksmithstore" component={BlacksmithStore}/>
                     <img src="https://cdn.pixabay.com/photo/2012/05/07/17/10/building-48716_960_720.png" 
                     className="gallery__img" 
                     alt="warehouse"/>
@@ -59,7 +61,7 @@ class Town extends React.Component{
                 </Switch>
 
             </div>   
-            </Router>    
+        </Router>    
             
         )
     }
