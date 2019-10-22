3c77ede16e33c5662ce8575ae53b4e6bbfb9cb3c
github username <github email address>
2019-10-22 10:21:01 -0700

    added monsters

--- idle_halloween_api/db/schema.rb
+++ idle_halloween_api/db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_10_21_180804) do
     t.string "name"
     t.integer "hp"
     t.integer "att"
+    t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
