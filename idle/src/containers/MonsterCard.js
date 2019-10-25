1eb753141c8141fb5f270f87ab96c96372d292dc
github username <github email address>
2019-10-25 14:17:03 -0700

    background

--- idle/src/containers/MonsterCard.js
+++ idle/src/containers/MonsterCard.js
@@ -5,7 +5,7 @@ import '../css/MonsterCard.css'
 export default class MonsterCard extends React.Component {
 
     renderHP=()=>{
-        return this.props.monster.hp > 0? `You slash ${this.props.monster.name} for 1 damage! ${this.props.monster.name} has ${this.props.monster.hp} remaining.` : `Monster defeated! You now have ${this.props.user.purse} gold`
+        // return this.props.monster.hp > 0? `You slash ${this.props.monster.name} for 1 damage! ${this.props.monster.name} has ${this.props.monster.hp} remaining.` : `Monster defeated! You now have ${this.props.user.purse} gold`
         if (this.props.fightStarted === false){
             return `${this.props.monster.hp} HP`
         } else if (this.props.fightStarted === true){
@@ -20,7 +20,7 @@ export default class MonsterCard extends React.Component {
                 <div className="card">
                     <h3 id="name">{this.props.monster.name}</h3>
                     <img id="img" alt="uhoh" src={this.props.monster.img} />
-                        <h4 id="hp"> {this.renderHP()}HP</h4>
+                        <h4 id="hp"> {this.renderHP()}</h4>
                     </div>
                     <div >
                  </div>
